91c211c97d78c59fd17d8beb561b349d6094f26f
quan1234 <quanvdph19781@fpt.edu.vn>
2023-07-14 22:23:41 +0700

    Create api AddProduct

--- src/router/product.js
+++ src/router/product.js
@@ -4,5 +4,7 @@ import { AddProduct } from '../controller/product';
 const router = express.Router();
 
 router.post('/products', AddProduct)
+router.delete('/products/:id', RemoveProduct)
+
 
 export default router
