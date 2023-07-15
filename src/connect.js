85a4bcbfe42933067c652ffe23ec0fb57e18594b
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-07-15 22:40:33 +0700

    fix

--- src/connect.js
+++ src/connect.js
@@ -3,7 +3,7 @@ const connect = new Client({
     user: 'postgres',
     password: 'admin',
     host: 'localhost',
-    port: 3001, // Cổng mặc định của PostgreSQL là 5432
+    port: 5432, // Cổng mặc định của PostgreSQL là 5432
     database: 'DU_AN(BAN_HANG)',
 });
 export default connect
--- src/controller/product.js
+++ src/controller/product.js
@@ -37,15 +37,15 @@ export const RemoveProduct = async (req, res, next) => {
 //getone
 export const GetOneProduct = async (req, res) => {
     try {
-        const id  = req.params.id
+        const id = req.params.id
         console.log(id);
         let sql = `SELECT * FROM products WHERE product_id=${id} `
         connect.query(sql, (err, result) => {
             if (err) {
-                return res.status(500).json({ message: 'Lay 1 san pham that bai',err })
+                return res.status(500).json({ message: 'Lay 1 san pham that bai', err })
             }
             const data = result.rows[0]
-            return res.status(200).json({ message: 'Lay 1 san pham thanh cong',data })
+            return res.status(200).json({ message: 'Lay 1 san pham thanh cong', data })
         })
     } catch (errr) {
         return res.status(500).json({ message: 'Loi api' })
@@ -56,19 +56,19 @@ export const GetOneProduct = async (req, res) => {
 
 
 //update
-export const UpdateProduct=async(req, res)=>{
-    try{
-        const id= req.params.id
-        const {name,price,quantity,color,image,description,cat_id} = req.body
+export const UpdateProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+        const { name, price, quantity, color, image, description, cat_id } = req.body
         let sql = `UPDATE products SET name='${name}', price=${price}, quantity=${quantity}, color = '${color}', image='${image}', description='${description}', cat_id = ${cat_id} WHERE product_id=${id} RETURNING *`
-        connect.query(sql,(err, results)=>{
-            if(err){
-                return res.status(500).json({message:'Update product that bai',err})
+        connect.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).json({ message: 'Update product that bai', err })
             }
             const data = results.rows[0]
-            return res.status(200).json({message:'Update product thanh cong',data})
+            return res.status(200).json({ message: 'Update product thanh cong', data })
         })
-    }catch(err){
-        return res.status(500).json({message:'Loi api'})
+    } catch (err) {
+        return res.status(500).json({ message: 'Loi api' })
     }
 }
