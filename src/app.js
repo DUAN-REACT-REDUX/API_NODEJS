ab54e3416b9fb97f59fd4b357cf56c9aba173b27
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-07-16 16:44:38 +0700

    create-api-signup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import connect from "./connect";
 import cors from "cors";
 import RouterCategories from "./router/category"
 import RouterProduct from "./router/product"
+import RouterUsers from "./router/user"
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 
 app.use('/api', RouterCategories)
 app.use('/api', RouterProduct)
+app.use('/api', RouterUsers)
 
 
 connect.connect((err) => {
