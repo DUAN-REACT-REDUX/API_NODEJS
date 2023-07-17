553201913489c9e7460d1aac31eef7d955abad16
vuthinhung1005 <nhungvt19868@fpt.edu.vn>
2023-07-18 02:47:09 +0700

    Create API Checkpermission

--- src/router/comments.js
+++ src/router/comments.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import { AddComment } from '../controller/comment';
+import express from "express";
+import { AddComment } from "../controller/comment";
+import { checkPermission } from "../middleware/checkPermission";
 
 const router = express.Router();
 
-router.post("/comments/add", AddComment)
+router.post("/comments/add", checkPermission, AddComment);
 
-export default router
+export default router;
