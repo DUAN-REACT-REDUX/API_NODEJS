69435b6721368090b9674704efc2ee039d7217ee
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-08-22 01:36:56 +0700

    project

--- src/connect.js
+++ src/connect.js
@@ -1,10 +1,10 @@
 import { Client } from 'pg'
 const connect = new Client({
     user: 'postgres',
-    password: 'ADMIN',
+    password: 'admin',
     host: 'localhost',
     port: 5432, // Cổng mặc định của PostgreSQL là 5432
     // port: 3001, // Cổng mặc định của PostgreSQL là 5432
-    database: 'DUAN',
+    database: 'Du_An',
 });
 export default connect
